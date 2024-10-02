3cf53fe324913c6d0bf8edf1a0ad1088f8568550
Steven LUCAS <steven.lucas@gxo.com>
2024-10-02 14:47:00 +0200

    Navbar, global theme with custom font and dark/light mode

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,171 @@
+'use client';
+
+import React, { useState, useEffect } from 'react';
+import { FaLinkedin, FaGithub, FaPhone, FaSun, FaMoon, FaBars, FaTimes } from 'react-icons/fa';
+import styles from './Navbar.module.scss';
+import Image from 'next/image';
+
+const Navbar = () => {
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const [theme, setTheme] = useState('light');
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 880);
+
+  const handleMobileToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const toggleTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+  };
+
+  useEffect(() => {
+    if (theme === 'dark') {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 880);
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize(); // Initial check
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, [theme]);
+
+  return (
+    <nav className={`${styles.navbar} ${mobileOpen ? styles.open : ''}`}>
+      <div className={styles.navContent}>
+        {/* Logo */}
+        <div className={styles.logo}>
+          <Image
+            src={
+              isMobile
+                ? theme === 'light'
+                  ? '/StivlucNoir.svg'
+                  : '/StivlucBlanc.svg'
+                : theme === 'light'
+                ? '/Logo.svg'
+                : '/WhiteLogo.svg'
+            }
+            alt='Logo'
+            width={isMobile ? 140 : 70}
+            height={40}
+            sizes='100vw'
+            priority
+          />
+        </div>
+        {/* Desktop Menu */}
+        <ul className={styles.desktopMenu}>
+          <li>
+            <a href='#presentation'>About me</a>
+          </li>
+          <li>
+            <a href='#projects'>Projects</a>
+          </li>
+          <li>
+            <a href='#diplomas'>Experience</a>
+          </li>
+          <li>
+            <a href='#contact'>Contact</a>
+          </li>
+        </ul>
+        {/* Right Side Icons */}
+        <div className={styles.rightIcons}>
+          {/* Theme Switcher */}
+          <button onClick={toggleTheme} className={`${styles.iconButton} ${styles.themeSwitcher}`}>
+            {theme === 'light' ? <FaSun /> : <FaMoon />}
+          </button>
+          {/* Separator */}
+          <div className={styles.separator}></div>
+          {/* Social Icons */}
+          <a
+            href='https://linkedin.com/in/stivluc'
+            target='_blank'
+            rel='noopener noreferrer'
+            className={`${styles.iconButton} ${styles.socialIcon}`}
+          >
+            <FaLinkedin />
+          </a>
+          <a
+            href='https://github.com/stivluc'
+            target='_blank'
+            rel='noopener noreferrer'
+            className={`${styles.iconButton} ${styles.socialIcon}`}
+          >
+            <FaGithub />
+          </a>
+          <a href='tel:+33688074187' className={`${styles.iconButton} ${styles.socialIcon}`}>
+            <FaPhone />
+          </a>
+          {/* Mobile Menu Button */}
+          <button className={styles.menuButton} onClick={handleMobileToggle}>
+            {mobileOpen ? <FaTimes /> : <FaBars />}
+          </button>
+        </div>
+      </div>
+      {/* Mobile Menu */}
+      {mobileOpen && (
+        <div className={styles.mobileMenu}>
+          <ul>
+            <li>
+              <a href='#presentation' onClick={handleMobileToggle}>
+                About me
+              </a>
+            </li>
+            <li>
+              <a href='#projects' onClick={handleMobileToggle}>
+                Projects
+              </a>
+            </li>
+            <li>
+              <a href='#diplomas' onClick={handleMobileToggle}>
+                Experience
+              </a>
+            </li>
+            <li>
+              <a href='#contact' onClick={handleMobileToggle}>
+                Contact
+              </a>
+            </li>
+          </ul>
+          <div className={styles.separator}></div>
+          <div className={styles.mobileSocialIcons}>
+            <a
+              href='https://linkedin.com/in/stivluc'
+              target='_blank'
+              rel='noopener noreferrer'
+              className={styles.mobileSocialIcon}
+            >
+              <FaLinkedin />
+              <span>LinkedIn</span>
+            </a>
+            <a
+              href='https://github.com/stivluc'
+              target='_blank'
+              rel='noopener noreferrer'
+              className={styles.mobileSocialIcon}
+            >
+              <FaGithub />
+              <span>GitHub</span>
+            </a>
+            <a href='tel:+33688074187' className={styles.mobileSocialIcon}>
+              <FaPhone />
+              <span>Call me !</span>
+            </a>
+          </div>
+          <div className={styles.mobileFooter}>
+            <p>
+              <span style={{ fontFamily: 'Helvetica, sans-serif' }}>©</span> {new Date().getFullYear()} Steven Lucas
+            </p>
+          </div>
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
