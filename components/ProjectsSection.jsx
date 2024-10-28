ab321d56dad8c33958604ec95ad46900227bb151
Steven LUCAS <steven.lucas@viacesi.fr>
2024-10-28 09:41:42 +0100

    projects done

--- components/ProjectsSection.jsx
+++ components/ProjectsSection.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+// ProjectsSection.jsx
+import React, { useState, useEffect } from 'react';
 import styles from './ProjectsSection.module.scss';
 import { motion } from 'framer-motion';
 import { projects } from '@/config/projectsContent';
+import Image from 'next/image';
+import { FiArrowUpRight } from 'react-icons/fi';
 
 const ProjectsSection = () => {
+  const [showAllProjects, setShowAllProjects] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 600);
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const displayedProjects = isMobile && !showAllProjects ? projects.slice(0, 2) : projects;
+
   return (
     <section className={styles.projectsContainer} id='projects'>
       <div className={styles.projects}>
@@ -17,29 +35,68 @@ const ProjectsSection = () => {
           Projects
           <span>Projects</span>
         </motion.h2>
+        <motion.p
+          initial={{ y: 30, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.6, delay: 0.5 }}
+          viewport={{ once: true, amount: 0.5 }}
+          className={styles.introText}
+        >
+          Here are some of the main projects I've completed.
+        </motion.p>
         <div className={styles.projectsGrid}>
-          {projects.map((project, index) => (
+          {displayedProjects.map((project, index) => (
             <motion.div
-              className={styles.projectCard}
+              className={`${styles.projectCard} ${project.href ? styles.clickable : ''}`}
               key={index}
               initial={{ y: 30, opacity: 0 }}
               whileInView={{ y: 0, opacity: 1 }}
               transition={{ duration: 0.4, delay: index * 0.2 }}
               viewport={{ once: true, amount: 0.5 }}
             >
-              <img src={project.image} alt={project.title} className={styles.projectImage} />
-              <h3 className={styles.projectTitle}>{project.title}</h3>
-              <p className={styles.projectDescription}>{project.description}</p>
               {project.href ? (
-                <a href={project.href} target='_blank' rel='noopener noreferrer' className={styles.projectLink}>
-                  Visit {new URL(project.href).hostname}
+                <a href={project.href} target='_blank' rel='noopener noreferrer' className={styles.projectLinkWrapper}>
+                  <div className={styles.imageWrapper}>
+                    <Image
+                      src={project.image}
+                      alt={project.title}
+                      width={400}
+                      height={300}
+                      className={styles.projectImage}
+                    />
+                  </div>
+                  <h3 className={styles.projectTitle}>{project.title}</h3>
+                  <p className={styles.projectDescription}>{project.description}</p>
+                  <span className={styles.projectLink}>
+                    {project.hrefText || `Visit ${new URL(project.href).hostname}`}{' '}
+                    <FiArrowUpRight className={styles.icon} />
+                  </span>
                 </a>
               ) : (
-                <span className={styles.projectNotOnline}>Not Online</span>
+                <div className={styles.projectLinkWrapper}>
+                  <div className={styles.imageWrapper}>
+                    <Image
+                      src={project.image}
+                      alt={project.title}
+                      layout='responsive'
+                      width={400}
+                      height={300}
+                      className={styles.projectImage}
+                    />
+                  </div>
+                  <h3 className={styles.projectTitle}>{project.title}</h3>
+                  <p className={styles.projectDescription}>{project.description}</p>
+                  <span className={styles.projectNotOnline}>{project.noHrefText || 'Not Online'}</span>
+                </div>
               )}
             </motion.div>
           ))}
         </div>
+        {isMobile && !showAllProjects && projects.length > 2 && (
+          <button onClick={() => setShowAllProjects(true)} className={styles.showMoreButton}>
+            ... and {projects.length - 2} more
+          </button>
+        )}
         <motion.p
           initial={{ y: 30, opacity: 0 }}
           whileInView={{ y: 0, opacity: 1 }}
@@ -47,8 +104,9 @@ const ProjectsSection = () => {
           viewport={{ once: true, amount: 0.5 }}
           className={styles.note}
         >
-          Due to client confidentiality, some projects are not displayed. <br />
-          Oh, and it also misses yours!
+          Due to client confidentiality, some projects are not displayed.
+          <br />
+          Oh, and yours could be next!
         </motion.p>
       </div>
     </section>
--- config/projectsContent.js
+++ config/projectsContent.js
@@ -2,32 +2,34 @@ export const projects = [
   {
     title: 'Next Hub',
     description:
-      'A complete BI solution for GXO Logistics, that centralizes data from different sources. It features advanced data analytics, productivity, live advancements. Local tools have also been developed to optimize logistics processes, while reducing repetitive tasks.',
+      'A BI solution for GXO Logistics centralizing data from various sources. Features advanced analytics, productivity tracking, and live updates.',
     image: '/GXO.png',
+    noHrefText: 'Private Project',
   },
   {
     title: 'Wink Monaco',
     description:
-      'Complete website for an association that help visually impaired. It contains a little shop, donation system (recurring or not), mass email sending system, or also forms to become volunteers. Some parts of the website are also editable through admin interface.',
+      'Website for an association aiding the visually impaired. Includes a shop, donation system, mass emails, volunteer forms, and admin interface.',
     image: '/WinkMonaco.png',
     href: 'https://www.wink-monaco.mc',
   },
   {
+    title: 'Lawyer Website',
+    description: 'Showcase website for a lawyer, including presentation, contact, and services sections.',
+    image: '/ChristineLucas.png',
+    href: 'https://www.christinelucas.fr',
+  },
+  {
     title: 'BuddyGames',
-    description: 'Board games rental services, featuring an admin interface to manage games, orders, users, etc. ',
+    description: 'Board games rental service with admin interface to manage games, orders, and users.',
     image: '/BuddyGames.png',
-    // No href
+    noHrefText: 'Company Closed',
   },
   {
     title: 'GamblEats',
     description:
-      'School project. Complete solution to order food and everytime you order: you got a chance for it to be free. Microservices architecture, featuring an app for each driver, restaurant and client.',
+      'Food ordering platform where each order gives a chance to get it free. Built with microservices architecture.',
     image: '/GamblEats.png',
-  },
-  {
-    title: 'Lawyer',
-    description: `First in-production project ever. Showcase website for a lawyer. Presentation, contact, services sections. It helped her gain +50% clients since it got in production.`,
-    image: '/ChristineLucas.png',
-    href: 'https://www.christinelucas.fr',
+    noHrefText: 'School Project',
   },
 ];
