a80a924a294faa795a1ee2c1fb29a3e89b776d1c
Steven LUCAS <steven.lucas2201@gmail.com>
2025-07-16 11:36:50 +0200

    changed job denomination

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -31,7 +31,7 @@ const jsonLd = {
   name: 'Steven Lucas',
   url: 'https://stivluc.com',
   sameAs: ['https://www.linkedin.com/in/stivluc', 'https://github.com/stivluc'],
-  jobTitle: 'Full-Stack Engineer',
+  jobTitle: 'Full Stack Engineer',
   worksFor: {
     '@type': 'Organization',
     name: 'Freelance',
@@ -43,18 +43,29 @@ export async function generateMetadata({ params: { locale } }) {
     title: locale === 'fr' ? 'Portfolio de Steven Lucas' : "Steven Lucas' Portfolio",
     description:
       locale === 'fr'
-        ? 'Portfolio de Steven Lucas, ingénieur full-stack spécialisé en React et Next.js. Disponible pour des projets freelance.'
-        : 'Portfolio of Steven Lucas, a full-stack engineer specializing in React and Next.js. Available for freelance projects.',
-    keywords: ['Steven Lucas', 'Full-Stack Engineer', 'React', 'Next.js', 'Portfolio', 'Freelance'],
+        ? 'Portfolio de Steven Lucas, ingénieur full stack spécialisé en React et Next.js. Disponible pour des projets freelance.'
+        : 'Portfolio of Steven Lucas, a full stack engineer specializing in React and Next.js. Available for freelance projects.',
+    keywords: [
+      'Steven Lucas',
+      'Full Stack Engineer',
+      'React',
+      'Next.js',
+      'Python',
+      'Django',
+      'Agile',
+      'MongoDB',
+      'Portfolio',
+      'Freelance',
+    ],
     authors: [{ name: 'Steven Lucas', url: 'https://stivluc.com' }],
     creator: 'Steven Lucas',
     metadataBase: new URL('https://stivluc.com'),
     openGraph: {
-      title: locale === 'fr' ? 'Steven Lucas | Ingénieur Full-Stack' : 'Steven Lucas | Full-Stack Engineer',
+      title: locale === 'fr' ? 'Steven Lucas | Ingénieur Full Stack' : 'Steven Lucas | Full Stack Engineer',
       description:
         locale === 'fr'
-          ? 'Portfolio de Steven Lucas, ingénieur full-stack spécialisé en React et Next.js. Disponible pour des projets freelance.'
-          : 'Portfolio of Steven Lucas, a full-stack engineer specializing in React and Next.js. Available for freelance projects.',
+          ? 'Portfolio de Steven Lucas, ingénieur full stack spécialisé en React et Next.js. Disponible pour des projets freelance.'
+          : 'Portfolio of Steven Lucas, a full stack engineer specializing in React and Next.js. Available for freelance projects.',
       url: 'https://stivluc.com',
       siteName: locale === 'fr' ? 'Portfolio de Steven Lucas' : 'Steven Lucas Portfolio',
       images: [
@@ -70,11 +81,11 @@ export async function generateMetadata({ params: { locale } }) {
     },
     twitter: {
       card: 'summary_large_image',
-      title: locale === 'fr' ? 'Steven Lucas | Ingénieur Full-Stack' : 'Steven Lucas | Full-Stack Engineer',
+      title: locale === 'fr' ? 'Steven Lucas | Ingénieur Full Stack' : 'Steven Lucas | Full Stack Engineer',
       description:
         locale === 'fr'
-          ? 'Portfolio de Steven Lucas, ingénieur full-stack spécialisé en React et Next.js. Disponible pour des projets freelance.'
-          : 'Portfolio of Steven Lucas, a full-stack engineer specializing in React and Next.js. Available for freelance projects.',
+          ? 'Portfolio de Steven Lucas, ingénieur full stack spécialisé en React et Next.js. Disponible pour des projets freelance.'
+          : 'Portfolio of Steven Lucas, a full stack engineer specializing in React and Next.js. Available for freelance projects.',
       images: ['/opengraph-image.jpg'],
     },
     icons: {
